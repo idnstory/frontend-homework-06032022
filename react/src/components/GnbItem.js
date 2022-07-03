38d42462279c43bde9dfb03004cc427aef26daee
오승익 <opp2555@naver.com>
2022-07-03 13:48:06 +0900

    react 작업 완료.

--- react/src/components/GnbItem.js
+++ react/src/components/GnbItem.js
@@ -1,8 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import 'scss/components/gnb.scss';
 
-const GnbItem = () => {
+const GnbItem = (gnbs) => {
   return (
-    <div className="gnb-item"></div>
+    <>
+      {gnbs.map((gnbOne) => {
+        return (
+          <div className="gnb-item" key={gnbOne.name} depth="1">
+            <span>{gnbOne.name}</span>
+            {gnbOne.children && gnbOne.children.map((gnbTwo) => {
+              return (
+                <div className="gnb-item" key={gnbTwo.name} depth="2">
+                  <span>{gnbTwo.name}</span>
+                  {gnbTwo.children && gnbTwo.children.map((gnbThree) => {
+                    return (
+                      <div className="gnb-item" key={gnbThree.name} depth="3">
+                        <span className="link">
+                          <Link to={gnbThree.pathname}>{gnbThree.name}</Link>
+                        </span>
+                      </div>
+                    )
+                  })}
+                </div>
+              );
+            })}
+          </div>      
+        )
+      })}
+    </>
   );
 };
 
